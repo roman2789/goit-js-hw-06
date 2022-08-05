ec4b074b6b21717f8d9876a97b357de364bdac23
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-05 14:41:52 +0300

    2nd task done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsListElem = document.querySelector("#ingredients");
+
+const ingredientsAllItemsElem = [];
+ingredients.forEach((element) => {
+  const ingredientsItemElem = document.createElement("li");
+  ingredientsItemElem.classList.add("item");
+  ingredientsItemElem.textContent = element;
+  ingredientsAllItemsElem.push(ingredientsItemElem);
+});
+
+ingredientsListElem.append(...ingredientsAllItemsElem);
+console.log(ingredientsListElem);
