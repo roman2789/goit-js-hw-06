41a29fd5ac56d44cf30f70f2e181b01b36b882c7
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-07 17:09:33 +0300

    task 8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+let formData = {};
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formElements = event.currentTarget.elements;
+//   !formElements.password.value || !formElements.email.value
+//     ? alert(`Всі поля повинні бути заповнені!`)
+//     : (formData.password = formElements.password.value) &&
+//       (formData.email = formElements.email.value);
+
+//   console.log(formData);
+// }
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  !formElements.password.value || !formElements.email.value
+    ? alert(`Всі поля повинні бути заповнені!`)
+    : (formData = new FormData(event.currentTarget));
+  formData.forEach((value, name) => {
+    console.log(`${name}: ${value}`);
+  });
+}
