779c3fcca83ba63d6b3776a722988eac143bb321
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-07 22:59:31 +0300

    add task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const changeBtnElem = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+
+changeBtnElem.addEventListener("click", changeColor);
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+}
+console.log();
