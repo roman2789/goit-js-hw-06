a02cc55eff60f78c9ce56042b3fb772855a4ebb0
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-07 13:39:44 +0300

    add task6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,7 +2,9 @@ const input = document.getElementById("validation-input");
 
 input.addEventListener("blur", onInputBlur);
 function onInputBlur(event) {
-  event.currentTarget.value.length === +input.dataset["length"]
-    ? (event.currentTarget.style.borderColor = "green")
-    : (event.currentTarget.style.borderColor = "red");
+  event.currentTarget.value.length !== +this.dataset["length"]
+    ? this.classList.add("invalid")
+    : this.classList.remove("invalid");
+  this.classList.add("valid");
+  // console.log(this);
 }
