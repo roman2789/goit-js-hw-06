930d7876371af17199662e7d0c4e9f276791e807
Roman Andrukhiv <andrukhivroman@gmail.com>
2022-08-08 19:26:37 +0300

    10 task done

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector("input");
+const createBtnElem = document.querySelector("[data-create]");
+const resetBtnElem = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+let totalBoxes = 0;
+
+input.addEventListener("input", (event) => {
+  totalBoxes = +event.currentTarget.value;
+});
+
+createBtnElem.addEventListener("click", onCreateBtnClick);
+resetBtnElem.addEventListener("click", onResetBtnClick);
+
+function onCreateBtnClick(amount) {
+  amount = totalBoxes;
+  let defaultSize = 30;
+  for (let i = 1; i <= amount; i++) {
+    let size = defaultSize + i * 10;
+    let box = document.createElement("div");
+
+    box.style = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};`;
+
+    boxes.appendChild(box);
+  }
+}
+function onResetBtnClick() {
+  boxes.innerHTML = "";
+  input.value = "";
+}
